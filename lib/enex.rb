265d35b0c0ba20074d3b4260357b3b95cd431c71
labocho <labocho@penguinlab.jp>
2018-07-19 13:59:30 +0900

    write Enex::Note#to_xml

--- lib/enex.rb
+++ lib/enex.rb
@@ -1,5 +1,63 @@
 require "enex/version"
 
+require "builder"
+require "base64"
+
 module Enex
-  # Your code goes here...
+  class Note
+    ATTRIBUTE_NAMES = [:export_date, :title, :content, :created, :updated, :tags, :resources]
+    attr_accessor *ATTRIBUTE_NAMES
+
+    def initialize(attributes = {})
+      attributes.each do |k, v|
+        raise "Unknown attribute: #{k.inspect}" unless ATTRIBUTE_NAMES.include?(k)
+        send("#{k}=", v)
+      end
+      self.tags ||= []
+      self.resources ||= []
+    end
+
+    def to_xml
+      x = Builder::XmlMarkup.new
+      x.instruct! :xml
+      x.declare! :DOCTYPE, :"en-export", :SYSTEM, "http://xml.evernote.com/pub/evernote-export3.dtd"
+      x.tag!("en-export", "export-date": time_to_s(export_date)){|x|
+        x.note{|x|
+          x.title title
+          x.content{|x| x.cdata! content }
+          x.created created if created
+          x.update updated if updated
+          tags.each{|tag| x.tag tag }
+          # TODO note-attributes
+          resources.each{|resource| x.resouce {|x| resource.build_xml_fragment(x) } }
+        }
+      }
+      x.target!
+    end
+
+    def time_to_s(t)
+      t.utc.strftime("%Y%m%dT%H%M%SZ")
+    end
+
+    class Resource
+      ATTRIBUTE_NAMES = [:data, :mime, :width, :height, :duration]
+      attr_accessor *ATTRIBUTE_NAMES
+
+      def initialize(attributes = {})
+        attributes.each do |k, v|
+          raise "Unknown attribute: #{k.inspect}" unless ATTRIBUTE_NAMES.include?(k)
+          send("#{k}=", v)
+        end
+      end
+
+      def build_xml_fragment(x)
+        x.data Base64.encode64(data), encoding: "base64"
+        x.mime mime
+        x.width width if width
+        x.height height if height
+        x.duration duration if duration
+        # TODO recognition, resource-attributes, alternate-data
+      end
+    end
+  end
 end
--- spec/enex_spec.rb
+++ spec/enex_spec.rb
@@ -3,7 +3,25 @@ RSpec.describe Enex do
     expect(Enex::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'Note' do
+    let(:note) {
+      Enex::Note.new(
+        title: "Title",
+        content: "<div>Content</div>",
+        export_date: Time.utc(2018, 7, 19, 12, 34, 56).getlocal,
+      )
+    }
+
+    describe 'to_xml' do
+      subject { note.to_xml }
+
+      it {
+        should == <<-XML
+          <?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-export SYSTEM "http://xml.evernote.com/pub/evernote-export3.dtd"><en-export export-date="20180719T123456Z"><note><title>Title</title><content><![CDATA[<div>Content</div>]]></content></note></en-export>
+        XML
+        .strip
+        File.write("test.enex", subject)
+      }
+    end
   end
 end
